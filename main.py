c66000af59cd17192042166407d243c4d55f0a26
Sergey Babichev <s.babichevv91p@gmail.com>
2022-06-28 16:17:57 +0300

    add booking_router, account_router to main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from hotel.routers import hotel_router
+from booking.routers import booking_router
+from account.routers import account_router
 
 app = FastAPI(title='hotel')
+
 app.include_router(hotel_router)
+app.include_router(booking_router)
+app.include_router(account_router)
