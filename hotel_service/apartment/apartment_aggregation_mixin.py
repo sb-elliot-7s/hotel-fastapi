d05c502c9f62484e0f732464bbb2b0d6de9d787a
Sergey Babichev <s.babichevv91p@gmail.com>
2022-07-01 15:03:34 +0300

    create functions for mongodb operations

--- hotel_service/apartment/apartment_aggregation_mixin.py
+++ hotel_service/apartment/apartment_aggregation_mixin.py
@@ -1,3 +1,6 @@
+from bson import ObjectId
+
+
 class AggregationMixin:
 
     @staticmethod
@@ -30,3 +33,55 @@ class AggregationMixin:
     @staticmethod
     def add_fields(new_name: str, operator: str, old_name: str):
         return {"$addFields": {new_name: {f"${operator}": f"${old_name}"}}}
+
+    @staticmethod
+    def filter_apartment(**params): return params
+
+    @staticmethod
+    def pull_item(**conditions): return {'$pull': {**conditions}}
+
+    @staticmethod
+    def set_document(document: dict): return {'$set': document}
+
+    @staticmethod
+    def push_item_to_array(**field_and_items): return {'$push': {**field_and_items}}
+
+    @staticmethod
+    def push_items_to_array(array_name: str, items: list, default=None):
+        return {'$push': {array_name: {'$each': items or default}}}
+
+    @staticmethod
+    def check_in(array_: list): return {'$in': array_}
+
+    @staticmethod
+    def expr_(condition: dict): return {'$expr': condition}
+
+    @staticmethod
+    def compare_values(operator: str, left_value, right_value):
+        return {f'${operator}': [left_value, right_value]}
+
+    @staticmethod
+    def inc_value(**fields):
+        """
+        :param fields: key: str value: int
+        :return: {'inc': {'key1': value1, 'key2': value2}
+        """
+        return {'$inc': {**fields}}
+
+    async def get_common_pipeline_for_find_apartments(self, skip: int, limit: int, _filter: dict):
+        return [
+            self.match(query=_filter),
+            self.add_fields(new_name='apt_id', operator='toString', old_name='_id'),
+            self.lookup(secondary_collection='review', secondary_collection_field='apartment_id',
+                        primary_collection_field='apt_id', _as='reviews'),
+            self.skip(value=skip),
+            self.limit(value=limit),
+            self.sort(created=-1),
+        ]
+
+    async def get_pipeline_for_detail_apartment(self, apartment_id: str):
+        return [
+            self.match(query={'_id': ObjectId(apartment_id)}),
+            self.add_fields(new_name='apt_id', operator='toString', old_name='_id'),
+            self.lookup('review', 'apartment_id', 'apt_id', 'reviews')
+        ]
--- hotel_service/apartment/apartment_repositories.py
+++ hotel_service/apartment/apartment_repositories.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 from typing import Optional
-
 from bson import ObjectId
-
 from database import client
 from .apartment_schemas import CreateApartmentSchema, ApartmentQuerySchema, \
     UpdateApartmentSchema, SearchApartmentSchema
@@ -21,13 +19,15 @@ class ApartmentRepositories(AggregationMixin, ApartmentRepositoriesInterface):
         self.__apartment_collection = apartment_collection
 
     async def __get_apartment(self, apartment_id: str):
-        if (apartment := await self.__apartment_collection.find_one({'_id': ObjectId(apartment_id)})) is None:
+        if (apartment := await self.__apartment_collection
+                .find_one(self.filter_apartment(_id=ObjectId(apartment_id)))) is None:
             raise_exception(status.HTTP_404_NOT_FOUND, 'Apartment not found')
         return apartment
 
     async def __update_apartment_after_images_saved(self, apartment_id: str, images_id: list[ObjectId]):
         return await self.__apartment_collection.find_one_and_update(
-            {'_id': ObjectId(apartment_id)}, {'$push': {'images': {'$each': images_id or []}}},
+            self.filter_apartment(_id=ObjectId(apartment_id)),
+            self.push_items_to_array(array_name='images', items=images_id, default=[]),
             return_document=True)
 
     async def __save_images(self, images: Optional[list[UploadFile]]):
@@ -35,13 +35,14 @@ class ApartmentRepositories(AggregationMixin, ApartmentRepositoriesInterface):
             return [await self.__image_service.write_image(image, image.filename) for image in images]
 
     async def __update_hotel(self, apartment, hotel_id: str, account_id: str):
-        data = {
-            'filter': {'_id': ObjectId(hotel_id), 'account_id': account_id,
-                       '$expr': {'$lt': [{'$size': '$apartments'}, '$count_of_apartments']}},
-            'update': {'$inc': {'available_count_of_apartments': 1}, '$push': {'apartments': apartment}},
-            'return_document': True
-        }
-        if (_ := await self.__hotel_collection.find_one_and_update(**data)) is None:
+        _filter = self.filter_apartment(
+            _id=ObjectId(hotel_id), account_id=account_id,
+            **self.expr_(condition=self.compare_values(
+                operator='lt', left_value={'$size': '$apartments'}, right_value='$count_of_apartments')))
+        if (_ := await self.__hotel_collection.find_one_and_update(_filter, update={
+            **self.inc_value(available_count_of_apartments=1),
+            **self.push_item_to_array(apartments=apartment)
+        }, return_document=True)) is None:
             raise_exception(status.HTTP_404_NOT_FOUND, 'Hotel not found or unable to add apartment to hotel')
 
     async def __create_apartment(self, account, apartment: CreateApartmentSchema,
@@ -59,78 +60,67 @@ class ApartmentRepositories(AggregationMixin, ApartmentRepositoriesInterface):
 
     async def create_apartment(self, account, apartment: CreateApartmentSchema,
                                images: Optional[list[UploadFile]] = None):
-        apt = await self.__create_apartment(account=account, apartment=apartment, images=images)
-        await self.__update_hotel(apartment=apt, hotel_id=apartment.hotel_id, account_id=account.id)
-        return apt
+        async with await client.start_session() as session:
+            async with session.start_transaction():
+                apt = await self.__create_apartment(account=account, apartment=apartment, images=images)
+                await self.__update_hotel(apartment=apt, hotel_id=apartment.hotel_id, account_id=account.id)
+                return apt
 
     async def remove_apartment(self, account, apartment_id: str):
-        apt_filter = {'_id': ObjectId(apartment_id), 'account_id': account.id}
-        if (apt := await self.__apartment_collection.find_one_and_delete(apt_filter)) is None:
-            raise_exception(status.HTTP_404_NOT_FOUND, 'Apartment not found')
-        await self.__hotel_collection.update_one({'_id': ObjectId(apt['hotel_id'])}, {
-            '$inc': {'available_count_of_apartments': -1},
-            '$pull': {'apartments': {'_id': apt['_id']}}
-        })
-        if apt['images']:
-            for image in apt['images']:
-                await self.__image_service.delete_image(image_id=image)
-        return apt
+        async with await client.start_session() as session:
+            async with session.start_transaction():
+                apt_filter = self.filter_apartment(_id=ObjectId(apartment_id), account_id=account.id)
+                if (apt := await self.__apartment_collection.find_one_and_delete(apt_filter)) is None:
+                    raise_exception(status.HTTP_404_NOT_FOUND, 'Apartment not found')
+                await self.__hotel_collection.update_one(
+                    filter=self.filter_apartment(_id=ObjectId(apt['hotel_id'])),
+                    update={
+                        **self.inc_value(available_count_of_apartments=-1),
+                        **self.pull_item(apartments={'_id': apt['_id']})
+                    }
+                )
+                if images := apt['images']:
+                    for image in images:
+                        await self.__image_service.delete_image(image_id=image)
+                return apt
 
     async def update_apartment(self, account, apartment_id: str,
                                apartment: UpdateApartmentSchema, images: Optional[list[UploadFile]] = None):
-
         images_id = await self.__save_images(images=images)
-        data = {
-            'filter': {'_id': ObjectId(apartment_id), 'account_id': account.id},
-            'update': {
-                '$set': apartment.transformed_dict,
-                '$push': {'images': {'$each': images_id or []}}
-            },
-            'return_document': True
-        }
-        if (apartment := await self.__apartment_collection.find_one_and_update(**data)) is None:
+        if (apartment := await self.__apartment_collection.find_one_and_update(
+                filter=self.filter_apartment(_id=ObjectId(apartment_id), account_id=account.id),
+                update={
+                    **self.set_document(document=apartment.transformed_dict),
+                    **self.push_items_to_array(array_name='images', items=images_id, default=[])
+                }, return_document=True)
+        ) is None:
             raise_exception(status.HTTP_404_NOT_FOUND, 'Apartment not found')
         return apartment
 
     async def detail_apartment(self, apartment_id: str):
-        pipeline = [
-            self.match(query={'_id': ObjectId(apartment_id)}),
-            self.add_fields(new_name='apt_id', operator='toString', old_name='_id'),
-            self.lookup('review', 'apartment_id', 'apt_id', 'reviews')
-        ]
+        pipeline = await self.get_pipeline_for_detail_apartment(apartment_id=apartment_id)
         if apt := (await self.__apartment_collection.aggregate(pipeline=pipeline).to_list(length=1))[0]:
             return apt
-
-    async def __get_common_pipeline(self, skip: int, limit: int, _filter: dict):
-        return [
-            self.match(_filter),
-            self.add_fields('apt_id', 'toString', '_id'),
-            self.lookup('review', 'apartment_id', 'apt_id', 'reviews'),
-            self.skip(skip),
-            self.limit(limit),
-            self.sort(created=-1),
-        ]
 
     async def all_available_apartments(self, hotel_id: str, query_data: ApartmentQuerySchema,
                                        skip: int, limit: int):
         query = ApartmentQueryService().prepare_query_data(data=query_data)
         query.update({'hotel_id': hotel_id})
-        pipeline = await self.__get_common_pipeline(skip=skip, limit=limit, _filter=query)
+        pipeline = await self.get_common_pipeline_for_find_apartments(skip=skip, limit=limit, _filter=query)
         return [apt async for apt in self.__apartment_collection.aggregate(pipeline=pipeline)]
 
     async def search(self, search_data: SearchApartmentSchema, skip: int, limit: int):
-        pipeline = await self.__get_common_pipeline(skip, limit, self.search_text(search_data.search_query))
-        cursor = self.__apartment_collection.aggregate(pipeline=pipeline)
-        return [apt async for apt in cursor]
+        query = self.search_text(search_data.search_query)
+        pipeline = await self.get_common_pipeline_for_find_apartments(skip=skip, limit=limit, _filter=query)
+        return [apt async for apt in self.__apartment_collection.aggregate(pipeline=pipeline)]
 
     async def delete_image(self, image_id: str, account):
         async with await client.start_session() as session:
             async with session.start_transaction():
                 await self.__image_service.delete_image(image_id=image_id)
-                filter_data = {'filter': {'account_id': account.id, 'images': {'$in': [ObjectId(image_id)]}}}
-                apartment = await self.__apartment_collection.find_one(**filter_data)
-                updated_data = {
-                    'filter': {'_id': apartment['_id']},
-                    'update': {'$pull': {'images': ObjectId(image_id)}}
-                }
-                await self.__apartment_collection.update_one(**updated_data)
+                filter_data = self.filter_apartment(
+                    account_id=account.id, images=self.check_in([ObjectId(image_id)]))
+                apartment = await self.__apartment_collection.find_one(filter_data)
+                await self.__apartment_collection \
+                    .update_one(filter=self.filter_apartment(_id=apartment['_id']),
+                                update=self.pull_item(images=ObjectId(image_id)))
